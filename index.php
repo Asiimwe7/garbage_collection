43ecc3790a5046d82bab275b789c20d01fb4759d
ninsiimawilber@gmail.com <ninsiimawilber@gmail.com>
2025-07-01 13:40:39 +0300

    add buttons

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Users</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container">
+        
+        <h1 class="mb-4">User List
+            <button type="button" class="btn btn-primary mx-5">create account</button>
+        </h1>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>wilber</td>
+                    <td>088788999</td>
+                    <td>
+                        <a href="" class="btn btn-primary">edit</a>
+                        <a href="" class="btn btn-danger">delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
